5cead97334a6bce0396344d6fcf6339d15e5af7c
Sara Yousry <sara.yousry@brightskiesinc.com>
2024-12-26 10:32:26 +0200

    Adjust Toolbar Style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-w-screen max-h-screen min-h-screen flex flex-col justify-between items-center bg-slate-200 ">
+  <div class="min-w-screen max-h-screen min-h-screen flex flex-col justify-between items-center bg-slate-200 overflow-hidden ">
     <ToolbarComponent v-if="!$route.meta.hideToolbar" />
     <RouterView />
   </div>
